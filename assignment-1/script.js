df4f528af3eec6d7251b4d66fa92776a6d803d96
adarshmaharjan <adarsh986030@gmail.com>
2021-11-23 17:31:10 +0545

    [removed comments] removed unessary codes

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -27,34 +27,3 @@ try {
     alert(`Please enter a valid inputs`, err.name)
     root.textContent = "Please enter a valid inputs"
 }
-
-//Inputs
-// let firstNum = 2
-// let secondNum = 3
-// let operation = "/"
-
-// show(firstNum, secondNum, operation)
-// console.log(firstNum, secondNum, operation)
-// const add = (firstNum, secondNum, operation) => {
-//     let result = firstNum + secondNum
-//     show(firstNum, secondNum, operation, result)
-// }
-// const sub = (firstNum, secondNum, operation) => {
-//     let result = firstNum - secondNum
-//     show(firstNum, secondNum, operation, result)
-// }
-// const multiply = (firstNum, secondNum, operation) => {
-//     let result = firstNum + secondNum
-//     show(firstNum, secondNum, operation, result)
-// }
-// const div = (firstNum, secondNum, operation) => {
-//     let result = (firstNum / secondNum).toFixed(3)
-//     show(firstNum, secondNum, operation, result)
-// }
-// if (operation === "+") add(firstNum, secondNum, operation)
-
-// if (operation === "-") sub(firstNum, secondNum, operation)
-
-// if (operation === "*") multiply(firstNum, secondNum, operation)
-
-// if (operation === "/") div(firstNum, secondNum, operation)
